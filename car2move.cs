f5317b86450bcd4c71f345eb65e94be0f6ed05bd
Sakifneoworld <you@example.com>
2018-04-01 12:05:23 +0600

    Message

---
+++ car2move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car2move : MonoBehaviour {
+
+    float speed = 3.0f;
+    float turnspeed = 20.0f;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(speed , 0, 0) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(-speed, 0, 0) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -turnspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, turnspeed * Time.deltaTime);
+        }
+
+
+
+    }
+}
