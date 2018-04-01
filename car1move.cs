3c7df0984329f9369efe3487183b536a0adbb7c4
Sakifneoworld <you@example.com>
2018-04-01 11:41:03 +0600

    Message

---
+++ car1move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car1move : MonoBehaviour {
+
+    float speed = 3.0f;
+
+    void Start () {
+
+	}
+	
+	void Update () {
+        transform.Translate(new Vector3(speed, 0, 0) * speed * Time.deltaTime);
+    }
+}
